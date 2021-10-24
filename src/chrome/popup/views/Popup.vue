98cf64b27f5d4b545d2a3a9005952bad05fb8a24
Zeffon <ZeffonWu@gmail.com>
2021-10-24 21:54:53 +0800

    chore: 引入vuedraggable

--- src/chrome/popup/views/Popup.vue
+++ src/chrome/popup/views/Popup.vue
@@ -1,7 +1,26 @@
+<!--
+ * @Description: popup
+ * @Author: Zeffon
+ * @Date: 2021-10-09 22:02:36
+ * @LastEditors: Zeffon
+ * @LastEditTime: 2021-10-24 20:34:34
+-->
 <template>
   <div class="g-popup">
     <div class="g-popup-header">当前任务（3）项</div>
-    <div class="g-popup-main">main</div>
+    <div class="g-popup-main">
+      <draggable
+        v-model="myArray"
+        group="people"
+        @start="drag = true"
+        @end="drag = false"
+        item-key="id"
+      >
+        <template #item="{ element }">
+          <div>{{ element }}</div>
+        </template>
+      </draggable>
+    </div>
     <div class="g-popup-footer">
       <div>编辑</div>
       <div>启动</div>
@@ -12,10 +31,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import draggable from 'vuedraggable'
 
 export default defineComponent({
   name: 'Index',
-  components: {}
+  components: {
+    draggable
+  },
+
+  data() {
+    return {
+      myArray: [1, 2, 3, 4],
+      drag: false
+    }
+  }
 })
 </script>
 
