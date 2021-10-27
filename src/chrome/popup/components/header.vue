235ba618e7c8d843e6ada89b04eee89626e20f24
Zeffon <ZeffonWu@gmail.com>
2021-10-27 21:40:49 +0800

    style: m-header

--- src/chrome/popup/components/header.vue
+++ src/chrome/popup/components/header.vue
@@ -3,13 +3,15 @@
  * @Author: Zeffon
  * @Date: 2021-10-24 22:30:07
  * @LastEditors: Zeffon
- * @LastEditTime: 2021-10-26 21:00:22
+ * @LastEditTime: 2021-10-27 21:36:20
 -->
 <template>
   <div class="m-header">
-    <div>已完成</div>
-    <div>当前（{{ curCount }}）</div>
-    <div>已完成</div>
+    <div class="m-header__item" @click="clickOne">已完成</div>
+    <div class="m-header__item is-active" @click="clickTwo">
+      当前（{{ curCount }}）
+    </div>
+    <div class="m-header__item" @click="clickThree">数据统计</div>
   </div>
 </template>
 
@@ -25,6 +27,17 @@ export default defineComponent({
       default() {
         return 0
       }
+    }
+  },
+  methods: {
+    clickOne() {
+      console.log(1)
+    },
+    clickTwo() {
+      console.log(2)
+    },
+    clickThree() {
+      console.log(3)
     }
   }
 })
