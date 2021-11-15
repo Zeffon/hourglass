b6bd4a442afe1bf5c0d6b82af5675770d9b1c8ad
Zeffon <ZeffonWu@gmail.com>
2021-11-15 09:12:18 +0800

    feat: 全局配置echarts

--- src/chrome/popup/index.ts
+++ src/chrome/popup/index.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import * as echarts from 'echarts';
 
 // 引入全局样式
 import '@/assets/scss/index.scss'
 
-createApp(App).use(store).mount('#popup')
+const app = createApp(App)
+app.config.globalProperties.echarts = echarts // 挂载echars
+
+app.use(store).mount('#popup')
